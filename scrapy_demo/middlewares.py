a50b6605ad05f1d6f00c904501d2b40b5b320be5
genie9700 <mathiasaustine9700@gmail.com>
2024-03-28 15:08:54 +0100

    Update middlewares.py

--- scrapy_demo/middlewares.py
+++ scrapy_demo/middlewares.py
@@ -9,7 +9,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-class ScrapyDemoSpiderMiddleware:
+class AdvancedcrawlerSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -53,10 +53,10 @@ class ScrapyDemoSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
-class ScrapyDemoDownloaderMiddleware:
+class AdvancedcrawlerDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
@@ -100,4 +100,4 @@ class ScrapyDemoDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
