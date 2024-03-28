2cc111acab2cb38e547c822bba3308dcb843a743
genie9700 <mathiasaustine9700@gmail.com>
2024-03-28 15:08:17 +0100

    Update items.py

--- scrapy_demo/items.py
+++ scrapy_demo/items.py
@@ -1,7 +1,39 @@
-from scrapy.item import Item, Field
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
 
-class QuoteItem(Item):
-    text = Field()
-    tags = Field()
-    author = Field()
-    test = Field()
+import scrapy
+
+
+class AdvancedcrawlerItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    meta_description = scrapy.Field()
+    main_content = scrapy.Field()
+    favicon_url = scrapy.Field()
+    entities = scrapy.Field()
+    sentiment = scrapy.Field()
+    site_name = scrapy.Field()
+    language = scrapy.Field()
+    publication_date = scrapy.Field()
+    internal_links = scrapy.Field()
+    heading_tags = scrapy.Field()
+    security_trustworthiness = scrapy.Field()
+    semantic_markup = scrapy.Field()
+    keywords = scrapy.Field()
+    named_entities = scrapy.Field()
+    summary = scrapy.Field()
+    readability_scores = scrapy.Field()
+    page_load_time = scrapy.Field()
+    num_outbound_links = scrapy.Field()
+    page_size = scrapy.Field()
+    response_headers = scrapy.Field()
+    user_interaction_elements = scrapy.Field()
+    ad_networks = scrapy.Field()
+    structured_markup_errors = scrapy.Field()
+    page_rank = scrapy.Field()
+    text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
+    # Add more fields as needed
